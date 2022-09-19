078f5f65f7072571bd59e02b6a6dfc2c3dc8324d
Emerie111 <emeriemadu00@gmail.com>
2022-09-19 10:59:19 -0700

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Puppet to make changes to our configuration file
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
